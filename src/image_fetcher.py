347e102d7824648bf99f1f80f4c04eaea1f278c7
AlexLeungZ <alexleungchunwai@gmail.com>
2023-04-10 04:30:32 +0800

    Update utils.py

--- src/image_fetcher.py
+++ src/image_fetcher.py
@@ -6,7 +6,7 @@ def main():
     gzip_path = './data/meta_AMAZON_FASHION.json.gz'
     url_col = "imageURLHighRes"
     uid_col = "asin"
-    utils.get_image(path, gzip_path, url_col, uid_col, 64)
+    utils.get_image(path, gzip_path, url_col, uid_col)
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,14 @@
-import concurrent.futures
+import concurrent.futures as futures
 import gzip
 import json
+import os
 from pathlib import Path
 
 import numpy as np
 import pandas as pd
 import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util import Retry
 
 
 def parse(path: str):
@@ -24,24 +27,31 @@ def get_df(path: str) -> pd.DataFrame:
     return dataframe
 
 
-def get_image(path: str, gzip_path: str, url_col: str, uid_col: str, num_workers: int = 1):
-    def get_url() -> list[pd.DataFrame]:
+def get_image(path: str, gzip_path: str, url_col: str, uid_col: str, num_workers: int = os.cpu_count() * 5):
+    def get_url() -> tuple[list[pd.DataFrame], int]:
         meta = get_df(gzip_path)
         meta = meta[[url_col, uid_col]].dropna()
         df_list = np.array_split(meta, num_workers)
-        return df_list
+        return df_list, meta.shape[0]
 
     def fetch_image(meta: pd.DataFrame):
         for url, uid in zip(meta[url_col], meta[uid_col]):
-            print(url[0], uid)
-            response = requests.get(url[0], timeout=60)
-            with open(path + f"{uid}.jpg", "wb") as file:
-                file.write(response.content)
+            fullpath = path + f"{uid}.{Path(url[0]).suffix}"
+            if not Path(fullpath).exists():
+                session = requests.Session()
+                session.mount('https://', HTTPAdapter(max_retries=Retry(total=5)))
+                response = session.get(url[0], timeout=120)
+                if response.status_code == 200:
+                    with open(fullpath, "wb") as file:
+                        file.write(response.content)
+                else:
+                    print(f"Missing {uid}")
 
     Path(path).mkdir(parents=True, exist_ok=True)
-    pool = concurrent.futures.ThreadPoolExecutor(max_workers=num_workers)
-    meta = get_url()
+    pool = futures.ThreadPoolExecutor(max_workers=num_workers)
+    meta, length = get_url()
 
+    print(f"Getting {length} images with {num_workers} workers")
     for i in range(num_workers):
         pool.submit(fetch_image(meta[i]))
     pool.shutdown(wait=True)
