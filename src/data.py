65ba5daacce91a4614a9db406e7c9e332dd631b5
AlexLeungZ <alexleungchunwai@gmail.com>
2023-04-19 20:37:50 +0800

    Modified model

---
+++ src/data.py
@@ -0,0 +1,73 @@
+import pandas as pd
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+
+from preprocess import preprocess_image, preprocess_text
+
+
+def preprocess_product_data(dataframe: pd.DataFrame, ncore: int):
+    stemmer = WordNetLemmatizer()
+    stop = stopwords.words("english")
+    img_size = (32, 32)
+    img_path = "./image"
+
+    cols = ["title", "brand", "description"]
+    dataframe.dropna(subset=cols, how="all", inplace=True)
+
+    stem = preprocess_text(dataframe, cols, ncore, stop, stemmer)
+    feat = preprocess_image(dataframe["asin"], img_size, img_path)
+
+    for col in cols:
+        dataframe[col] = stem[col]
+
+    result = dataframe[["asin"]].copy(deep=True)
+    result["wordpool"] = dataframe[cols].fillna("").agg(" ".join, axis=1)
+    result["image"] = feat
+
+    result.dropna(subset="image", inplace=True)
+    result["image"] = StandardScaler().fit_transform(result["image"].to_list()).tolist()
+
+    return result
+
+
+def preprocess_user_data(dataframe: pd.DataFrame, ncore: int):
+    stemmer = WordNetLemmatizer()
+    stop = stopwords.words("english")
+
+    cols = ["reviewText", "summary"]
+    dataframe = dataframe.groupby("asin").filter(lambda x: len(x) >= 5)
+    dataframe = dataframe.groupby("reviewerID").filter(lambda x: len(x) >= 2)
+    dataframe.dropna(subset=cols, how="all", inplace=True)
+    dataframe.sort_values(by=["unixReviewTime"], inplace=True)
+
+    # rate_under = dataframe[dataframe['overall'] != 5]
+    # rate_length = round(rate_under.groupby("overall").count()["asin"].mean())
+    # rate_five = dataframe[dataframe['overall'] == 5].sample(n=rate_length)
+    # dataframe = pd.concat([rate_under, rate_five], ignore_index=True)
+
+    stem = preprocess_text(dataframe, cols, ncore, stop, stemmer)
+    for col in cols:
+        dataframe[col] = stem[col]
+
+    result = dataframe[["asin", "reviewerID", "overall"]].copy(deep=True)
+    result["reviewpool"] = dataframe[cols].fillna("").agg(" ".join, axis=1)
+
+    return result
+
+
+def preprocess_merge_data(product_data: pd.DataFrame, user_data: pd.DataFrame):
+    merge = pd.merge(user_data, product_data, on='asin', how="inner")
+
+    cols = ["reviewpool", "wordpool"]
+    merge["text"] = merge[cols].fillna("").agg(" ".join, axis=1)
+    merge.drop(columns=cols, inplace=True)
+
+    merge["text"] = list(TfidfVectorizer().fit_transform(merge["text"]).toarray())
+    # merge["text"] = list(TfidfVectorizer(max_features=64).fit_transform(merge["text"]).toarray())
+
+    merge['asin'] = LabelEncoder().fit_transform(merge['asin'])
+    merge['reviewerID'] = LabelEncoder().fit_transform(merge['reviewerID'])
+
+    return merge
--- src/test.py
+++ src/test.py
@@ -1,72 +1,57 @@
 import os
 
+import numpy as np
 import pandas as pd
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.preprocessing import StandardScaler
+from keras.callbacks import EarlyStopping
+from keras.utils import plot_model
+from sklearn.model_selection import train_test_split
 
-from preprocess import preprocess_text, preprocess_image
-from utils import get_image, get_df, sampling_df
-
-
-def preprocess_product_data(dataframe: pd.DataFrame, ncore: int):
-    stemmer = WordNetLemmatizer()
-    stop = stopwords.words("english")
-    img_size = (32, 32)
-    img_path = "./image"
-
-    cols = ["title", "brand", "description"]
-    dataframe.dropna(subset=cols, how="all", inplace=True)
-
-    stem = preprocess_text(dataframe, cols, ncore, stop, stemmer)
-    feat = preprocess_image(dataframe["asin"], img_size, img_path)
-
-    for col in cols:
-        dataframe[col] = stem[col]
-
-    result = dataframe[["asin"]].copy(deep=True)
-    result["wordpool"] = dataframe[cols].fillna('').agg(' '.join, axis=1)
-    result["image"] = feat
-
-    result.dropna(subset="image", inplace=True)
-    # result["wordpool"] = TfidfVectorizer().fit_transform(result['wordpool']).toarray().tolist()
-    result["image"] = StandardScaler().fit_transform(result["image"].to_list()).tolist()
-
-    return result
-
-
-def preprocess_user_data(dataframe: pd.DataFrame, ncore: int):
-    stemmer = WordNetLemmatizer()
-    stop = stopwords.words("english")
-
-    cols = ["reviewText", "summary"]
-    dataframe = dataframe.groupby("asin").filter(lambda x: len(x) >= 5)
-    dataframe = dataframe.groupby("reviewerID").filter(lambda x: len(x) >= 2)
-    result = dataframe.dropna(subset=cols, how="all").sort_values(by=['unixReviewTime'])
-
-    stem = preprocess_text(result, cols, ncore, stop, stemmer)
-    for col in cols:
-        dataframe[col] = stem[col]
-
-    result = result[["asin"]].copy(deep=True)
-    result["reviewpool"] = dataframe[cols].fillna('').agg(' '.join, axis=1)
-
-    return result
+from data import preprocess_merge_data, preprocess_product_data, preprocess_user_data
+from model import hybrid_model
+from utils import get_df, get_image, sampling_df
 
 
 def main():
     ncore = os.cpu_count()
+    test_size = 0.2
+    data_frac = 0.01
+    emb_size = 32
+    do_hybird = True
+    epochs = 100
+    batch_size = 32
+
     product_data = get_df("./data/meta_AMAZON_FASHION.json.gz")
-    # user_data = get_df("./data/AMAZON_FASHION.json.gz")
+    user_data = get_df("./data/AMAZON_FASHION.json.gz")
 
     # get_nltk_resource()
-    # product_data = sampling_df(product_data, 0.02)
+    product_data = sampling_df(product_data, data_frac)
     product_data = preprocess_product_data(product_data, ncore)
-    product_data.to_csv("product_data.csv", index=False)
+    # product_data.to_csv("product_data_temp.csv", index=False)
 
-    # user_data = preprocess_user_data(user_data, ncore)
-    # user_data.to_csv("user_data.csv", index=False)
+    user_data = preprocess_user_data(user_data, ncore)
+    # user_data.to_csv("user_data_temp.csv", index=False)
+
+    merge_data = preprocess_merge_data(product_data, user_data)
+    merge_data.to_csv("temp.csv", index=False)
+
+    train_data, test_data = train_test_split(merge_data, test_size=test_size)
+    num_user = merge_data['asin'].to_numpy().size
+    num_prod = np.unique(merge_data['asin']).size
+
+    text_vec_size = len(merge_data["text"][0])
+    img_vec_size = len(merge_data["image"][0])
+
+    model = hybrid_model(num_user, num_prod, text_vec_size, img_vec_size, emb_size, do_hybird)
+    plot_model(model, to_file='model_structure.png', show_shapes=True, show_layer_names=True)
+    stooper = EarlyStopping(monitor='val_loss', mode='min', verbose=0, patience=50)
+
+    cols = ["reviewerID", "asin", "text", "image"]
+    train_set = [np.stack(train_data[col], 0) for col in cols]
+    test_set = [np.stack(test_data[col], 0) for col in cols]
+
+    history = model.fit(train_set, train_data["overall"], validation_split=0.1, epochs=epochs, batch_size=batch_size,
+                        verbose=2, shuffle=True, use_multiprocessing=True, workers=ncore, callbacks=[stooper])
+    predict = model.predict(test_set, verbose=0, use_multiprocessing=True, workers=ncore)
 
 
 if __name__ == "__main__":
